16059745713beb445dc9f35577f9bf7ec4c27dd4
Marcio Schlichting <marcioosergioo2015@gmail.com>
2023-03-31 18:20:17 -0300

    Creating the Dropdown tsx

---
+++ dhruva-mobile/src/components/Dropdown/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+type DropdownProps = {
+    label: string
+    items: Array<DropdownItem>
+    onSelect: 
+}
+
+type DropdownItem = {
+    label: string
+    value: string
+}
+
+const Dropdown = ({ 
+    label, 
+    visible } : DropdownProps) =>{
+        const [visible, setVisible] = 
+
+    }
+
+
+export default Dropdown;
