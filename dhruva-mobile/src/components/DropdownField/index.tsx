9a002f82add38f57f514dc761dd616b6ec681f33
Marcio Schlichting <marcioosergioo2015@gmail.com>
2023-04-03 22:53:39 -0300

    Adding dropdown component

---
+++ dhruva-mobile/src/components/DropdownField/index.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+const data = [
+    { label: 'Item 1', value: '1' },
+    { label: 'Item 2', value: '2' },
+    { label: 'Item 3', value: '3' },
+    { label: 'Item 4', value: '4' },
+    { label: 'Item 5', value: '5' },
+    { label: 'Item 6', value: '6' },
+    { label: 'Item 7', value: '7' },
+    { label: 'Item 8', value: '8' },
+];
+
+const DropdownField = () => {
+    const [value, setValue] = useState<string>()
+    const [isFocused, setIsFocused] = useState<boolean>(false)
+
+    return (<View>
+        <Dropdown
+          style={[styles.dropdown, isFocused && { borderColor: 'blue' }]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={data}
+          search
+          maxHeight={300}
+          labelField="label"
+          valueField="value"
+          placeholder={!isFocused ? 'Select item' : '...'}
+          searchPlaceholder="Search..."
+          value={value}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onChange={item => {
+            setValue(item.value);
+            setIsFocused(false);
+          }}
+          renderLeftIcon={() => (
+            <AntDesign
+              style={styles.icon}
+              color={isFocused ? 'blue' : 'black'}
+              name="Safety"
+              size={20}
+            />
+          )}
+        />
+    </View>)
+}
+
+export default DropdownField;
+
+const styles = StyleSheet.create({
+    dropdown: {
+      margin: 16,
+      height: 50,
+      borderBottomColor: 'gray',
+      borderBottomWidth: 0.5,
+    },
+    icon: {
+      marginRight: 5,
+    },
+    placeholderStyle: {
+      fontSize: 16,
+    },
+    selectedTextStyle: {
+      fontSize: 16,
+    },
+    iconStyle: {
+      width: 20,
+      height: 20,
+    },
+    inputSearchStyle: {
+      height: 40,
+      fontSize: 16,
+    },
+  });
--- dhruva-mobile/src/screens/ProfileScreen/index.tsx
+++ dhruva-mobile/src/screens/ProfileScreen/index.tsx
@@ -1,8 +1,9 @@
 import { View, Text } from "react-native";
+import DropdownField from "../../components/DropdownField";
 
 const ProfileScreen = () : JSX.Element => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', width: '100%' }}>
+        <DropdownField/>
     </View>
 );
 
