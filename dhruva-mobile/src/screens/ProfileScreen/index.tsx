ae59cf768d081e79421c6bc48c44980fb01d50e5
Marcio Schlichting <marcioosergioo2015@gmail.com>
2023-04-03 22:28:38 -0300

    Removing dropdown element

--- dhruva-mobile/src/screens/ProfileScreen/index.tsx
+++ dhruva-mobile/src/screens/ProfileScreen/index.tsx
@@ -2,9 +2,7 @@ import { View, Text } from "react-native";
 
 const ProfileScreen = () : JSX.Element => (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 16, fontWeight: '700' }}>
-            Refer Screenasd
-        </Text>
+        
     </View>
 );
 
